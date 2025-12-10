5ddb6bebfdb3a415a643361f95bec4cc34b44ca7
rajatjaiswal2001 <rajat.jaiswalmgs2@gmail.com>
2025-12-10 11:59:53 +0530

    weather_app
    remove extra code

--- weather_app.py
+++ weather_app.py
@@ -37,16 +37,3 @@ def get_weather(city):
     return weather
 
 
-# ------------------------------------
-# TESTING
-# ------------------------------------
-if _name_ == "_main_":
-    city_name = input("Enter city name: ")
-    result = get_weather(city_name)
-
-    print("\nReal-Time Weather:")
-    if isinstance(result, str):
-        print(result)
-    else:
-        for key, value in result.items():
-            print(f"{key}: {value}")
